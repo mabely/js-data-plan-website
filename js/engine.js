3e49d92a181759cedb98936cf3add2cb3f398704
Mabel Li <contact@mabel.li>
2018-11-20 16:49:24 +0000

    First attempt at JavaScript with buttons.

---
+++ js/engine.js
@@ -0,0 +1,20 @@
+var gb = ["3GB", "6GB", "15GB", "30GB"];
+var prices = ['£15', '£17', '£22', '£27'];
+
+$('button').click(function(event){
+  var wine = this.getAttribute("value");
+  console.log("anything");
+  // $('#play_you').text(play_you);
+  // var play_machine = computer_move();
+  // compare(play_you, play_machine);
+  // score(verdict);
+});
+
+
+var gb = prices();
+for (i=0; i<3; i++){
+gb[i] = prices[i]
+return gb;
+}
+
+//console.log(gb);
